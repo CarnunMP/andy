f23fae7e3094f748e14c6e41f2eab2e261b94c44
CarnunMP <admin@carnun.com>
2023-10-10 11:20:03 +0100

    Relativise `andy-notes-urls.edn` path

--- bin/andy.clj
+++ bin/andy.clj
@@ -6,9 +6,7 @@
 (import java.io.PushbackReader)
 
 
-(def abs-base "Documents/Coding/Clojure-Randoms/andy/")
-
-(with-open [r (io/reader (str abs-base "resources/andy-notes-urls.edn"))]
+(with-open [r (io/reader (io/resource "andy-notes-urls.edn"))]
   (let [rand-url (rand-nth (edn/read (PushbackReader. r)))]
     (println (str "opening " rand-url))
     (sh "xdg-open" rand-url)))
