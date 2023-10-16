1e349c182e5e6144190fe09da088e0cb5c179cfb
CarnunMP <admin@carnun.com>
2023-10-16 12:21:30 +0100

    Fix filepath
    It's now good both for REPLing and following the symlink, detailed in
    the README.

--- bin/andy.clj
+++ bin/andy.clj
@@ -1,12 +1,18 @@
 #!/usr/bin/env bb
 
-(require '[clojure.edn :as edn]
+(require '[babashka.fs :as fs]
+         '[clojure.edn :as edn]
          '[clojure.java.io :as io]
-         '[clojure.java.shell :refer [sh]])
+         '[clojure.java.shell :refer [sh]]
+         '[clojure.string :as str])
 (import java.io.PushbackReader)
 
+(def f (if (str/blank? *file*) ; i.e. REPLing
+         "../resources/andy-notes-urls.edn"
+         ;; follow the symlink (see README.md)
+         (-> *file* fs/real-path fs/parent fs/parent (fs/path "resources/andy-notes-urls.edn") str)))
 
-(with-open [r (io/reader (io/resource "andy-notes-urls.edn"))]
+(with-open [r (io/reader f)]
   (let [rand-url (rand-nth (edn/read (PushbackReader. r)))]
     (println (str "opening " rand-url))
     (sh "xdg-open" rand-url)))
